90319a0fefcd448b701607655d1cb33ecbeb9c2b
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-16 13:16:33 +0100

    first commit

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,18 @@
+<!-- @format -->
+
+<template>
+  <div class="welcome container">
+    <h1>Welcome Page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
