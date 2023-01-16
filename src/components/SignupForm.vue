4eb586699509a1c6b3b0ac67dc88d6fc9b514653
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-16 14:33:42 +0100

    Created Signup component

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,35 @@
+<!-- @format -->
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input
+      type="text"
+      required
+      placeholder="display name"
+      v-model="displayName"
+    />
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    // ref
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = () => {
+      console.log(displayName.value, email.value, password.value);
+    };
+
+    return { displayName, email, password, handleSubmit };
+  },
+};
+</script>
+
+<style scoped></style>
